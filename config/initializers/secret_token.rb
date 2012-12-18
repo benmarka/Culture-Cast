5e339eab054c2aaf1e9e4c72cf4b136ab2ce638d
Aaron Benmark <benmarkaa@gmail.com>
2012-12-18 14:52:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CultureCast::Application.config.secret_token = '6248b9d0ae77d0cdf03ac211e67986c04bd0614bdb9e2a19a694607b44e6e97f5a3589a04ef2e73dc7402d4e4b783e523e68f691c1ab83991b28c68e1914e6e8'
